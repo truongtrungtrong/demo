0846a694768b0c46041828caf090069ea2332424
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-06-02 08:57:12 +0700

    test add item

--- src/main/java/view/XemLichGUI.java
+++ src/main/java/view/XemLichGUI.java
@@ -3,12 +3,14 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.sql.Date;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 
+import com.mindfusion.common.DateTime;
 import com.mindfusion.scheduling.Calendar;
+import com.mindfusion.scheduling.model.Appointment;
 
 @SuppressWarnings("serial")
 public class XemLichGUI extends JFrame {
@@ -18,6 +20,12 @@ public class XemLichGUI extends JFrame {
 		khoiTaoFrame();
 	}
 	private void khoiTaoFrame() {
+		DateTime d = new DateTime(2021, 6, 1);
+		Appointment app = new Appointment();
+		app.setStartTime(d);
+		app.setEndTime(d);
+		app.setHeaderText("Lớp 12345\tGV 12345 Buoi sang");
+		calendar.getSchedule().getItems().add(app);
 		setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.fill = GridBagConstraints.BOTH;
